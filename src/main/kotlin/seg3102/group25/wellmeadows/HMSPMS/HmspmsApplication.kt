6d4f9513b1a18fdf0cc734247dba12fc78ccec37
GreyLake <bigrasj3@gmail.com>
2023-10-17 16:27:04 -0400

    JB - Added Template Spring Application (W/D: Lombok, Spring Web, Thymeleaf)

---
+++ src/main/kotlin/seg3102/group25/wellmeadows/HMSPMS/HmspmsApplication.kt
@@ -0,0 +1,11 @@
+package seg3102.group25.wellmeadows.HMSPMS
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HmspmsApplication
+
+fun main(args: Array<String>) {
+	runApplication<HmspmsApplication>(*args)
+}
---
+++ src/test/kotlin/seg3102/group25/wellmeadows/HMSPMS/HmspmsApplicationTests.kt
@@ -0,0 +1,13 @@
+package seg3102.group25.wellmeadows.HMSPMS
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HmspmsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
