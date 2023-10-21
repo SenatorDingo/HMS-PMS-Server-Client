1999eadd022eda4606eec29d137242ab0d01a6e3
HQin036 <hqin036@uottawa.ca>
2023-10-21 14:26:27 -0400

    Initial Commit to DTOs

--- src/main/kotlin/seg3102/group25/wellmeadows/hmspms/application/dtos/queries/PerscribeMedicationDTO.kt
+++ src/main/kotlin/seg3102/group25/wellmeadows/hmspms/application/dtos/queries/PerscribeMedicationDTO.kt
@@ -1,4 +1,18 @@
 package seg3102.group25.wellmeadows.hmspms.application.dtos.queries
 
-class PerscribeMedicationDTO {
-}
+import java.time.LocalDate
+import java.time.LocalTime
+
+data class PrescribeMedicationDTO(
+    var doctorId: String,
+    var patientId: String,
+    var drugNumber: String,
+    var drugName: String,
+    var unitsPerDay: Int,
+    var numberOfAdministrationsPerDay: Int,
+    var administrationTimes: List<LocalTime>,
+    var unitsAtAdministrationTimes: List<Int>,
+    var methodOfAdministration: String,
+    var startDate: LocalDate,
+    var finishDate: LocalDate
+)
--- src/main/kotlin/seg3102/group25/wellmeadows/hmspms/application/dtos/queries/RequestPatientAdmissionDTO.kt
+++ src/main/kotlin/seg3102/group25/wellmeadows/hmspms/application/dtos/queries/RequestPatientAdmissionDTO.kt
@@ -1,4 +1,10 @@
 package seg3102.group25.wellmeadows.hmspms.application.dtos.queries
 
-class RequestPatientAdmissionDTO {
-}
+data class RequestPatientAdmissionDTO(
+    var chargeNurseId: String,
+    var patientId: String,
+    var divisionId: String,
+    var rationaleForRequest: String,
+    var priorityAssessment: Int, // on a scale of 1 to 10
+    var localDoctor: String
+)
