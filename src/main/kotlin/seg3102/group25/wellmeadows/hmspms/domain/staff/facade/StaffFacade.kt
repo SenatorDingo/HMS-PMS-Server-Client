eae607623d7487724150307223bf57f728da10d5
GreyLake <bigrasj3@gmail.com>
2023-11-09 18:56:40 -0500

    JB - Renamed Staff Role to StaffType

--- src/main/kotlin/seg3102/group25/wellmeadows/hmspms/domain/staff/facade/StaffFacade.kt
+++ src/main/kotlin/seg3102/group25/wellmeadows/hmspms/domain/staff/facade/StaffFacade.kt
@@ -1,18 +1,17 @@
 package seg3102.group25.wellmeadows.hmspms.domain.staff.facade
 
 import seg3102.group25.wellmeadows.hmspms.application.dtos.queries.RegisterStaffDTO
-import seg3102.group25.wellmeadows.hmspms.application.usecases.RegisterStaff
 import seg3102.group25.wellmeadows.hmspms.domain.staff.entities.account.StaffAccount
-import seg3102.group25.wellmeadows.hmspms.domain.staff.valueObjects.Role
+import seg3102.group25.wellmeadows.hmspms.domain.staff.valueObjects.StaffType
 
 interface StaffFacade {
     fun createStaffAccount(accountInfo: RegisterStaffDTO): Boolean
     fun updateStaffAccount(employeeNumber: String, accountInfo: RegisterStaffDTO): Boolean
     fun updatePassword(employeeNumber: String, password: String): Boolean
-    fun addRole(employeeNumber: String, role: Role): Boolean
-    fun removeRole(employeeNumber: String, role: Role): Boolean
-    fun updateRoles(employeeNumber: String, roles: List<Role>): Boolean
-    fun getRoles(employeeNumber: String): List<Role>
+    fun addRole(employeeNumber: String, type: StaffType): Boolean
+    fun removeRole(employeeNumber: String, type: StaffType): Boolean
+    fun updateRoles(employeeNumber: String, types: List<StaffType>): Boolean
+    fun getRoles(employeeNumber: String): List<StaffType>
     fun addFacilityID(employeeNumber: String, facilityID: String): Boolean
     fun removeFacilityID(employeeNumber: String, facilityID: String): Boolean
     fun updateFacilityIDs(employeeNumber: String, facilityIDs: List<String>): Boolean
--- src/main/kotlin/seg3102/group25/wellmeadows/hmspms/domain/staff/valueObjects/StaffType.kt
+++ src/main/kotlin/seg3102/group25/wellmeadows/hmspms/domain/staff/valueObjects/StaffType.kt
@@ -1,6 +1,6 @@
 package seg3102.group25.wellmeadows.hmspms.domain.staff.valueObjects
 
-enum class Role {
+enum class StaffType {
     MedicalDirector,
     PersonnelOfficer,
     LocalDoctor,
