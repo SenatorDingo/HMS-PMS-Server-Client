436ea5cc5e657a9c5ceaf85d016013e45303fc35
GreyLake <bigrasj3@gmail.com>
2023-11-10 20:30:14 -0500

    JB - Added Patient WIP Facade

--- src/main/kotlin/seg3102/group25/wellmeadows/hmspms/domain/patient/facade/implementation/PatientFacadeImpl.kt
+++ src/main/kotlin/seg3102/group25/wellmeadows/hmspms/domain/patient/facade/implementation/PatientFacadeImpl.kt
@@ -1,4 +1,41 @@
 package seg3102.group25.wellmeadows.hmspms.domain.patient.facade.implementation
 
-class PatientFacadeImpl {
+import seg3102.group25.wellmeadows.hmspms.application.dtos.queries.PrescribeMedicationDTO
+import seg3102.group25.wellmeadows.hmspms.application.dtos.queries.RegisterPatientDTO
+import seg3102.group25.wellmeadows.hmspms.domain.patient.entities.file.PatientFile
+import seg3102.group25.wellmeadows.hmspms.domain.patient.entities.prescription.PatientPrescription
+import seg3102.group25.wellmeadows.hmspms.domain.patient.facade.PatientFacade
+
+class PatientFacadeImpl: PatientFacade {
+    override fun createPatientFile(patientDTO: RegisterPatientDTO): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun updatePatientFile(patientDTO: RegisterPatientDTO): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun getPatientFile(patientNumber: String): PatientFile? {
+        TODO("Not yet implemented")
+    }
+
+    override fun createPatientPrescription(prescriptionDTO: PrescribeMedicationDTO): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun updatePatientPrescription(prescriptionDTO: PrescribeMedicationDTO): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun getPatientPrescriptions(patientNumber: String): List<PatientPrescription> {
+        TODO("Not yet implemented")
+    }
+
+    override fun admitPatient(patientNumber: String): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun dischargePatient(patientNumber: String): Boolean {
+        TODO("Not yet implemented")
+    }
 }
