17171e9f2fbccfe7cef7870ee8ddbfd40893896a
Josiah Bigras <44141334+GreyLake@users.noreply.github.com>
2023-11-15 18:42:28 -0500

    JB - Update FacilityAdmissionWaitListFactoryStub.kt

--- src/test/kotlin/seg3102/group25/wellmeadows/hmspms/contracts/testStubs/factories/FacilityAdmissionWaitListFactoryStub.kt
+++ src/test/kotlin/seg3102/group25/wellmeadows/hmspms/contracts/testStubs/factories/FacilityAdmissionWaitListFactoryStub.kt
@@ -1,6 +1,6 @@
 package seg3102.group25.wellmeadows.hmspms.contracts.testStubs.factories
 
-import seg3102.group25.wellmeadows.hmspms.application.dtos.queries.AdmitPatientRequestListDTO
+import seg3102.group25.wellmeadows.hmspms.application.dtos.queries.RequestPatientAdmissionDTO
 import seg3102.group25.wellmeadows.hmspms.domain.facility.entities.admissionWaitList.FacilityAdmissionWaitList
 import seg3102.group25.wellmeadows.hmspms.domain.facility.factories.FacilityAdmissionWaitListFactory
 import java.util.Date
