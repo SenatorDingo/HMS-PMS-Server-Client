952bfd658c00af7d4fc67fed486e7255eecfa706
GreyLake <bigrasj3@gmail.com>
2023-11-22 17:55:31 -0500

    JB - Added Request for Staff Account Login

--- src/main/kotlin/seg3102/group25/wellmeadows/hmspms/infrastructure/configuration/WebSecurityConfig.kt
+++ src/main/kotlin/seg3102/group25/wellmeadows/hmspms/infrastructure/configuration/WebSecurityConfig.kt
@@ -2,6 +2,7 @@ package seg3102.group25.wellmeadows.hmspms.infrastructure.configuration
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.annotation.Order
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
@@ -11,10 +12,28 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import seg3102.group25.wellmeadows.hmspms.domain.security.valueObjects.AccessLevels
+import seg3102.group25.wellmeadows.hmspms.infrastructure.web.services.StaffAuthenticationProvider
+import java.util.*
 
 @Configuration
-class WebSecurityConfig() {
+class WebSecurityConfig{
+
+    val superRole = arrayOf("ADMIN")
+
+    val admitPatientRole = AccessLevels.AdmitPatient.staffType.map { it.name }.toTypedArray()
+    val admitPatientRequestListRole = AccessLevels.AdmitPatientRequestList.staffType.map { it.name }.toTypedArray()
+    val consultPatientFileRole = AccessLevels.ConsultPatientFile.staffType.map { it.name }.toTypedArray()
+    val dischargePatientRole = AccessLevels.DischargePatient.staffType.map { it.name }.toTypedArray()
+    val prescribeMedicationRole = AccessLevels.PrescribeMedication.staffType.map { it.name }.toTypedArray()
+    val registerPatientRole = AccessLevels.RegisterPatient.staffType.map { it.name }.toTypedArray()
+    val registerStaffRole = AccessLevels.RegisterStaff.staffType.map { it.name }.toTypedArray()
+    val requestPatientAdmissionRole = AccessLevels.RequestPatientAdmission.staffType.map { it.name }.toTypedArray()
+    val updatePatientFileRole = AccessLevels.UpdatePatientFile.staffType.map { it.name }.toTypedArray()
+    val visualizeDivisionRole = AccessLevels.VisualizeDivision.staffType.map { it.name }.toTypedArray()
+
     @Bean
+    @Order(1)
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http
             .authorizeHttpRequests{ requests -> requests
@@ -28,8 +47,30 @@ class WebSecurityConfig() {
             .logout { logout -> logout
                 .permitAll()
             }
+            .userDetailsService(userDetailService())
 
         return http.build()
+    }
+
+
+    @Bean
+    @Order(2)
+    fun filterStaffChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .authorizeHttpRequests { requests -> requests
+                .requestMatchers("/actions/admit-patient").hasAnyRole(*superRole, *admitPatientRole)
+                .requestMatchers("/actions/admit-patient-request-list").hasAnyRole(*superRole, *admitPatientRequestListRole)
+                .requestMatchers("/actions/consult-patient").hasAnyRole(*superRole, *consultPatientFileRole)
+                .requestMatchers("/actions/discharge-patient").hasAnyRole(*superRole, *dischargePatientRole)
+                .requestMatchers("/actions/prescribe-medication").hasAnyRole(*superRole, *prescribeMedicationRole)
+                .requestMatchers("/actions/register-patient").hasAnyRole(*superRole, *registerPatientRole)
+                .requestMatchers("/actions/register-staff").hasAnyRole(*superRole, *registerStaffRole)
+                .requestMatchers("/actions/request-patient-admission").hasAnyRole(*superRole, *requestPatientAdmissionRole)
+                .requestMatchers("/actions/update-patient-file").hasAnyRole(*superRole, *updatePatientFileRole)
+                .requestMatchers("/actions/visualize-division").hasAnyRole(*superRole, *visualizeDivisionRole)
+            }
+            .authenticationProvider(StaffAuthenticationProvider())
+        return  http.build()
     }
 
     @Bean
