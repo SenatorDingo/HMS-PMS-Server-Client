bc28d00131f04a8221031722f531a8c1e042527a
SenatorDingo <abhayabhilash@duck.com>
2023-12-05 14:45:50 -0500

    AA - Controllers
    Added new empty controllers
    ACTIONS CONTROLLER IS STILL ACTIVE

---
+++ src/main/kotlin/seg3102/group25/wellmeadows/hmspms/infrastructure/web/controllers/actions/WebConsultPatientFileController.kt
@@ -0,0 +1,13 @@
+package seg3102.group25.wellmeadows.hmspms.infrastructure.web.controllers.actions
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Controller
+import seg3102.group25.wellmeadows.hmspms.domain.patientManagement.facade.PatientManagementFacade
+
+@Controller
+class WebConsultPatientFileController {
+
+    @Autowired
+    lateinit var patientManagementFacade: PatientManagementFacade
+
+}
